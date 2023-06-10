4a50e0e2a493c267a8b8ce8cd05d061ed0b59058
Emin Öztürk <eminozturk963@gmail.com>
2023-06-10 23:43:34 +0300

    Mesaj listeleme API'si güncellendi

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -3,27 +3,53 @@ const ChannelUser = require('../models/channelUser')
 const Message = require('../models/message')
 
 const get_message = async (req, res) => {
-    const  { channelID } = req.body
+    const { channelID } = req.body;
     const token = req.headers.authorization.split(' ')[1];
-    const userID = await currentUserID(token)
-    console.log(channelID, userID)
+    const userID = await currentUserID(token);
+    console.log(channelID, userID);
 
-    const channelUser = await ChannelUser.findOne({ 
-            channelID: "647b7dd61a7e5f4bd102659c", 
-            userID: userID 
-        }).select('createdAt deletedAt -_id')
-    const startDate = channelUser.createdAt
-    const endDate = channelUser.deletedAt == null ? Date.now() : channelUser.deletedAt
-    Message.find({ 
-        channelID: "647b7dd61a7e5f4bd102659c", 
-        createdAt: { $gt: startDate, $lt:  endDate } 
-    })  
-    .then((messages) => { res.json({ messages }) })
-    .catch((e) => { res.json({ status: false, error: e }) })
-}
+    const channelUser = await ChannelUser.findOne({
+        channelID: "647b7dd61a7e5f4bd102659c",
+        userID: userID
+    }).select('createdAt deletedAt -_id');
+
+    const startDate = channelUser.createdAt;
+    const endDate = channelUser.deletedAt == null ? Date.now() : channelUser.deletedAt;
+
+    Message.find({
+        channelID: "647b7dd61a7e5f4bd102659c",
+        createdAt: { $gt: startDate, $lt: endDate }
+    })
+        .populate({
+            path: 'senderID',
+            select: 'name surname'
+        })
+        .select('senderID content createdAt')
+        .then((messages) => {
+            const formattedMessages = messages.map((message) => {
+                return {
+                    id: message._id,
+                    sender: {
+                        id: message.senderID._id,
+                        name: message.senderID.name,
+                        surname: message.senderID.surname
+                    },
+                    content: message.content,
+                    createdAt: message.createdAt
+                };
+            });
+
+            res.json({ messages: formattedMessages });
+        })
+        .catch((e) => {
+            res.json({ status: false, error: e });
+        });
+};
+
+
 
 const post_create_message = (req, res) => {
-    const  { channelID, content } = req.body
+    const { channelID, content } = req.body
     const currentUserID = req.res.locals.currentUserID
     const message = new Message()
     message.senderID = currentUserID
